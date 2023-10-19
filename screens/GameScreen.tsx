f71c1de279f5265087cadbffeb7d654f5af9486a
Alexey Popov <akpop10@mts.ru>
2023-10-20 00:34:08 +0300

    4.18 add title and update screens

--- screens/GameScreen.tsx
+++ screens/GameScreen.tsx
@@ -1,6 +1,23 @@
 import { FC } from 'react';
-import { Text } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
+import { Title } from '../components/Title';
 
 export const GameScreen: FC = () => {
-  return <Text>Game Screen!</Text>;
+  return (
+    <View style={styles.screen}>
+      <Title>Opponent's Guess</Title>
+      {/*GUESS*/}
+      <View>
+        <Text>Higher or Lower?</Text>
+        {/*+ -*/}
+      </View>
+    </View>
+  );
 };
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    padding: 24,
+  },
+});
