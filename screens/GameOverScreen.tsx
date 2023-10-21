0d59de2848f0e9f7862ccea40f39b8b43d747f73
Alexey Popov <akpop10@mts.ru>
2023-10-21 22:52:15 +0300

    4.27 add styles and content to game over screen

--- screens/GameOverScreen.tsx
+++ screens/GameOverScreen.tsx
@@ -1,5 +1,37 @@
-import { Text } from 'react-native';
+import { Image, StyleSheet, View, Text } from 'react-native';
+import { Title } from '../components/ui/Title';
+import { Colors } from '../constants/colors';
 
 export const GameOverScreen = () => {
-  return <Text>GameOverScreen</Text>;
+  return (
+    <View style={styles.rootContainer}>
+      <Title>Game Over!</Title>
+      <View style={styles.imageContainer}>
+        <Image style={styles.image} source={require('../assets/images/success.jpg')} />
+      </View>
+      <Text>Your phone needed X rounds to guess the number Y.</Text>
+    </View>
+  );
 };
+
+const styles = StyleSheet.create({
+  rootContainer: {
+    flex: 1,
+    padding: 24,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  imageContainer: {
+    width: 300,
+    height: 300,
+    borderRadius: 150,
+    borderWidth: 3,
+    borderColor: Colors.primary800,
+    overflow: 'hidden',
+    margin: 36,
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+  },
+});
