7550e047a960721197858010bb0ca5d259c588af
Alexey Popov <akpop10@mts.ru>
2023-10-21 17:49:01 +0300

    4.24 extend styles props custom components

--- components/ui/InstructionText.tsx
+++ components/ui/InstructionText.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { ViewStyle } from 'react-native/Libraries/StyleSheet/StyleSheetTypes';
 import { Colors } from '../../constants/colors';
 
 interface Props {
   children: string;
+  style?: ViewStyle;
 }
 
-export const InstructionText = ({ children }: Props) => {
+export const InstructionText = ({ children, style }: Props) => {
   return (
     <View>
-      <Text style={styles.instructionText}>{children}</Text>
+      <Text style={[styles.instructionText, style]}>{children}</Text>
     </View>
   );
 };
--- screens/GameScreen.tsx
+++ screens/GameScreen.tsx
@@ -56,10 +56,14 @@ export const GameScreen = ({ userNumber, onGameOver }: GameScreenProps) => {
       <Title>Opponent's Guess</Title>
       <NumberContainer>{currentGuess}</NumberContainer>
       <Card>
-        <InstructionText>Higher or Lower?</InstructionText>
-        <View>
-          <PrimaryButton onPress={nextGuessHandler.bind(null, 'lower')}>-</PrimaryButton>
-          <PrimaryButton onPress={nextGuessHandler.bind(null, 'greater')}>+</PrimaryButton>
+        <InstructionText style={styles.instructionText}>Higher or Lower?</InstructionText>
+        <View style={styles.buttonsContainer}>
+          <View style={styles.buttonContainer}>
+            <PrimaryButton onPress={nextGuessHandler.bind(null, 'lower')}>-</PrimaryButton>
+          </View>
+          <View style={styles.buttonContainer}>
+            <PrimaryButton onPress={nextGuessHandler.bind(null, 'greater')}>+</PrimaryButton>
+          </View>
         </View>
       </Card>
     </View>
@@ -70,5 +74,14 @@ const styles = StyleSheet.create({
   screen: {
     flex: 1,
     padding: 24,
+  },
+  instructionText: {
+    marginBottom: 12,
+  },
+  buttonsContainer: {
+    flexDirection: 'row',
+  },
+  buttonContainer: {
+    flex: 1,
   },
 });
